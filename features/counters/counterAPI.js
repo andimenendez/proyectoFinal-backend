bd921145b3061d625bd29e96701691fe839893c0
Ignacio Velazquez <ignaciov92@gmail.com>
2023-04-27 22:42:29 -0300

    Limpieza del repositorio

--- features/counters/counterAPI.js
+++
@@ -1,7 +0,0 @@
-//función simulada para imitar la realización de una solicitud asíncrona de datos
-export function fetchCount(amount = 1) {
-    return new Promise((resolve) =>
-      setTimeout(() => resolve({ data: amount }), 500)
-    );
-  }
-  
