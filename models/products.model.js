a7a99428aeefbb644f020aeaa8aa4bc264eb6c14
Andrea Menendez <andreaveronicam96@gmail.com>
2023-04-29 15:54:24 -0300

    modelo de productos

---
+++ models/products.model.js
@@ -0,0 +1,31 @@
+const {Schema,model} = require('mongoose');
+
+const productSchema = Schema ({
+    nombre:{
+        type:String,
+        require:[true,"el nombre es necesario"]
+    } ,
+    precio:{
+        type:Number,
+        require:[true,"el precio es necesario"]
+    }, 
+    description: {
+        type:String,
+        required: false
+    },
+    disponible:{
+        type:Boolean,
+        require:true,
+        default:true
+    },
+    categoria:{
+        type:String,
+        require:true
+    },
+    imagenes:{
+        type:String,
+        require:false
+    }
+});
+
+module.exports = model('products',productSchema);
--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require ('express');
 const app = express();
 require('dotenv/config');
+require('../dataBase/dbConnection');
 const port = process.env.PORT;
 const morgan = require('morgan');
 const cors = require('cors');
 const productsRoutes = require('../routes/products.routes');
-
 
 //Middleware
 app.use(express.json());
