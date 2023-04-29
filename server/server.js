b573a824d66cac999fc93d6c5421e18045f21a47
santiago <santiagoroma89@gmail.com>
2023-04-29 08:48:35 -0300

    Modificaciones en el backend de productos. Controladores para cargar, eliminar, modificar, etc.

--- server/server.js
+++ server/server.js
@@ -5,6 +5,13 @@ const port = process.env.PORT;
 const morgan = require('morgan');
 const cors = require('cors');
 const products.routes = require('../routes/products.routes')
+const mongoose = require('mongoose');
+
+try {
+    mongoose.connect('mongodb://127.0.0.1:27017/testAMcreación')
+console.log('C. exitosa.')
+}
+
 
 //Middleware
 app.use(express.json());
@@ -13,6 +20,7 @@ app.use(cors());
 
 //Rutas
 app.use('/products', products.routes)
+
 
 app.listen(port,()=>{
     console.log(`estamos escuchando el puerto ${port}`);
---
+++ services/products.service.js
@@ -0,0 +1,29 @@
+const getProduct = async ()=> {
+
+};
+
+const getByIds = async ()=> { 
+
+};
+ 
+const createProduct = async ()=> {
+
+};
+
+const editProduct = async ()=> {
+
+};
+
+const deleteProduct = async ()=> {
+
+};
+
+module.exports = {
+    getProduct,
+    getByIds,
+    createProduct,
+    editProduct,
+    deleteProduct,
+}
+
+//Los products.service se diferencian de los product controllers por no tener la "s" en productS.
