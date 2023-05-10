26b33b7b5fb1ea8effbf12dfa1cc08ca4694474b
Ignacio Velazquez <ignaciov92@gmail.com>
2023-05-10 16:13:30 -0300

    reparando search backend

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,19 +1,31 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const route = Router();
-const { getAllProducts, getProductById, createProducts, editProducts, deleteProducts,disableProduct,destacarProducto,getDestacado } = require('../controllers/products.controllers');
+const {
+  getAllProducts,
+  getProductById,
+  createProducts,
+  editProducts,
+  deleteProducts,
+  disableProduct,
+  destacarProducto,
+  getDestacado,
+  getproductByName,
+} = require("../controllers/products.controllers");
 
-route.get('/get-all-products', getAllProducts );
+route.get("/get-all-products", getAllProducts);
 
-route.get('/get-by-id/:id', getProductById);
+route.get("/get-by-product/:name", getproductByName);
 
-route.post('/create-products', createProducts);
+route.get("/get-by-id/:id", getProductById);
 
- route.patch('/edit-products/:id', editProducts);
- 
- route.patch ('/disable-products/:id',disableProduct);
- 
- route.delete('/delete-products/:id', deleteProducts);
- 
- route.patch ('/destacar-producto/:id',destacarProducto);
-  
+route.post("/create-products", createProducts);
+
+route.patch("/edit-products/:id", editProducts);
+
+route.patch("/disable-products/:id", disableProduct);
+
+route.delete("/delete-products/:id", deleteProducts);
+
+route.patch("/destacar-producto/:id", destacarProducto);
+
 module.exports = route;
--- services/products.service.js
+++ services/products.service.js
@@ -1,33 +1,39 @@
-const Product = require('../models/products.model');
+const Product = require("../models/products.model");
 
-const getAllProductService = async ()=> {
-    return await Product.find();
+const getAllProductService = async () => {
+  return await Product.find();
 };
 
-const getProductByIdService = async (id)=> { 
-    return Product.findById(id);
-};
- 
-const createProductService = async (products)=> {
-    const newProduct = new Product(products);
-    return await newProduct.save();
+const getProductByIdService = async (id) => {
+  return Product.findById(id);
 };
 
-const editProductService = async (id,productData)=> {
-    return Product.findByIdAndUpdate(id,productData);
+const createProductService = async (products) => {
+  const newProduct = new Product(products);
+  return await newProduct.save();
 };
 
-const deleteProductService = async (id)=> {
-    return Product.findByIdAndDelete(id);
+const editProductService = async (id, productData) => {
+  return Product.findByIdAndUpdate(id, productData);
 };
 
+const deleteProductService = async (id) => {
+  return Product.findByIdAndDelete(id);
+};
+
+const obtenerProductoPorNombre = async (nombre) => {
+  const regex = new RegExp(nombre, "i");
+
+  return await Product.find({ nombre: { $regex: regex } });
+};
 
 module.exports = {
-    getAllProductService,
-    getProductByIdService,
-    createProductService,
-    editProductService,
-    deleteProductService,
-}
+  getAllProductService,
+  getProductByIdService,
+  createProductService,
+  editProductService,
+  deleteProductService,
+  obtenerProductoPorNombre,
+};
 
 //Los products.service se diferencian de los product controllers por no tener la "s" en productS.
