dad53ca5837ce041965d936ec221ed3e818b5c03
Andrea Menendez <andreaveronicam96@gmail.com>
2023-05-11 16:34:55 -0300

    acepte cambios produts

--- services/products.service.js
+++ services/products.service.js
@@ -1,45 +1,31 @@
-const Product = require("../models/products.model");
+const Product = require('../models/products.model');
 
-const getAllProductService = async () => {
-  return await Product.find();
+const getAllProductService = async ()=> {
+    return await Product.find();
 };
 
-const getProductByIdService = async (id) => {
-  return Product.findById(id);
+const getProductByIdService = async (id)=> { 
+    return Product.findById(id);
+};
+ 
+const createProductService = async (products)=> {
+    const newProduct = new Product(products);
+    return await newProduct.save();
 };
 
-const createProductService = async (products) => {
-  const newProduct = new Product(products);
-  return await newProduct.save();
+const editProductService = async (id,productData)=> {
+    return Product.findByIdAndUpdate(id,productData);
 };
 
-const editProductService = async (id, productData) => {
-  return Product.findByIdAndUpdate(id, productData);
+const deleteProductService = async (id)=> {
+    return Product.findByIdAndDelete(id);
 };
 
-const deleteProductService = async (id) => {
-  return Product.findByIdAndDelete(id);
-};
-
-// const obtenerProductoPorNombre = async (nombre) => {
-//   const regex = new RegExp(nombre, "i");
-
-//   return await Product.find({ nombre: { $regex: regex } });
-// };
-
-const obtenerProductoPorNombre = async (nombre) => {
-  const regex = new RegExp(nombre, "i");
-  const productos = await Product.find({ nombre: { $regex: regex } });
-  return productos;
-};
 
 module.exports = {
-  getAllProductService,
-  getProductByIdService,
-  createProductService,
-  editProductService,
-  deleteProductService,
-  obtenerProductoPorNombre,
-};
-
-//Los products.service se diferencian de los product controllers por no tener la "s" en productS.
+    getAllProductService,
+    getProductByIdService,
+    createProductService,
+    editProductService,
+    deleteProductService,
+}
